6606c6513ed9b930f8d556dbde3cf0e278b786ea
Esdras Pavon <pavonesdras@gmail.com>
2019-04-16 19:47:17 -0300

    Post and postlist component created

---
+++ src/components/PostList.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import Post from "./Post";
+class PostList extends Component {
+  showPosts = () => {
+    const posts = this.props.posts;
+    if (posts.length === 0) return null;
+    return (
+      <React.Fragment>
+        {Object.keys(posts).map(post => (
+          <Post key={post} data={this.props.posts[post]} />
+        ))}
+      </React.Fragment>
+    );
+  };
+
+  render() {
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Titulo</th>
+            <th scope="col">Acciones</th>
+          </tr>
+        </thead>
+        <tbody>{this.showPosts()}</tbody>
+      </table>
+    );
+  }
+}
+
+export default PostList;
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import PostList from "./PostList";
+
 class Posts extends Component {
   state = {};
   render() {
-    return <p>Desde posts</p>;
+    return (
+      <div className="col-12 col-md-8">
+        <h2 className="text-center">Posts</h2>
+        <PostList posts={this.props.posts} />
+      </div>
+    );
   }
 }
 
